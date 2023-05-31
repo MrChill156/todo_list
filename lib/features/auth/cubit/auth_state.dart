f959af5e3cc2827dd7f988a480aba2aeab2bae47
MrChill93 <phamthanhtung15693@gmail.com>
2023-05-31 15:21:26 +0700

    update all

--- lib/features/auth/cubit/auth_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'auth_cubit.dart';
-
-abstract class AuthState {
-  const AuthState();
-}
-
-class UnAuthenticated extends AuthState {}
-
-class Authenticated extends AuthState {}
--- lib/features/splash/cubit/splash_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'splash_cubit.dart';
-
-class SplashState extends Equatable {
-  const SplashState();
-
-  @override
-  List<Object> get props => [];
-}
